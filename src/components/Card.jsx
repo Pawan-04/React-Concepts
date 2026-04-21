f4aa8f34d9f0258de17c7a5214aa6a65d88e3e2c
Pawan Kumar Pandey <pawan4kp@gmail.com>
2026-04-22 02:48:51 +0530

    Card.jsx dynamic content using MAP

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 
 const Card = () => {
+
+  var arr = [{ img:"https://images.unsplash.com/photo-1773332585749-5146862ba746?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",title: "Beautiful Landscape", description: "This is a stunning image of a natural landscape featuring mountains and a serene lake. Perfect for relaxation and inspiration." },
+
+    { img:"https://images.unsplash.com/photo-1769788873128-442998aed3da?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",title: "Drive Towards Mountains ", description: "This is a stunning image of a natural landscape featuring mountains and a serene lake. Perfect for relaxation and inspiration." },
+
+    { img:"https://images.unsplash.com/photo-1772311283542-1c537cff0887?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxN3x8fGVufDB8fHx8fA%3D%3D",title: "Lights in the City", description: "This is a stunning image of a natural landscape featuring mountains and a serene lake. Perfect for relaxation and inspiration." }
+  ]
   return (
     <div className="flex space-x-4">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-        <img className="w-full h-48 object-cover" src="https://images.unsplash.com/photo-1773332585749-5146862ba746?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="Sample Image" />
+
+      {arr.map((elem,index)=>( <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img className="w-full h-48 object-cover" src={elem.img} alt="Sample Image" />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Beautiful Landscape</div>
+          <div className="font-bold text-xl mb-2">{elem.title}</div>
           <p className="text-gray-700 text-base">
-            This is a stunning image of a natural landscape featuring mountains and a serene lake. Perfect for relaxation and inspiration.
+           {elem.description}
           </p>
         </div>
-      </div>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-        <img className="w-full h-48 object-cover" src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW5zfGVufDB8fDB8fHww" alt="Mountain View" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Majestic Mountains</div>
-          <p className="text-gray-700 text-base">
-            Explore the breathtaking views of towering mountains covered in snow. A perfect destination for adventure seekers.
-          </p>
-        </div>
-      </div>
+      </div>))}
+     
+      
     </div>
   )
 }
