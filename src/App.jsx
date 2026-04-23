a4a6bead63605621ce4a783e165ebfed9790a778
Pawan Kumar Pandey <pawan4kp@gmail.com>
2026-04-23 14:46:30 +0530

    Card2.jsx - PROPS Complex handling states on button click 'Add Fiends' -card making -Mini Project

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,34 @@ function App() {
   const [data, setData] = useState([
   {img: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d",
     name: "Aman",
-    profession: "Artist"
+    profession: "Artist",
+    friend:false
   },
   {img: "https://images.unsplash.com/photo-1492724441997-5dc865305da7",
     name: "John",
-    profession: "Designer"
+    profession: "Designer",
+    friend:false
   },
   {img: "https://images.unsplash.com/photo-1776255076699-d2dc33434b16?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D",
     name: "Jane",
-    profession: "Developer"
+    profession: "Developer",
+    friend:false
   }
 ])
+
+const handleFriends = (cardIndex)=>{
+  setData((prev)=>{
+    return prev.map((item,i)=>{
+      if(i == cardIndex){
+        item = {...item,friend:!item.friend}
+      }
+      return item;
+    })
+  })
+}
   return (
     <div className="flex justify-center items-center min-h-screen">
-      {data.map((item,index)=> (<Card2 key={index} props={item}/>))}
+      {data.map((item,index)=> (<Card2 key={index} index={index} props={item} handleFriends={handleFriends} />))}
     </div>
   )
 }
--- src/components/Card2.jsx
+++ src/components/Card2.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-function Card2(props) {
-    const {img,name,profession} = props.props
+function Card2({props,index,handleFriends}) {
+    const {img,name,profession,friend} = props
   return (
     <div className="w-60 rounded-md border border-gray-400 shadow-lg">
-        <img src={img} className="w-full h-40 rounded-t-md" />
+        <img src={img} className="w-full h-40 rounded-t-md object-cover" />
       <div className=' flex flex-col gap-1 p-1 '>
         <h1 className='font-bold text-lg'>{name}</h1>
       <h3 className='text-gray-500 font-semibold'>{profession}</h3>
 
-      <button className="bg-blue-500 text-white py-1 px-4 rounded-md hover:bg-blue-600"
-      onClick={()=>alert("Added")}>Add Friend</button></div>
+      <button className={`bg-blue-500 text-white py-1 px-4 rounded-md hover:bg-blue-600 transition ${friend ? 'bg-green-500 hover:bg-green-600' : ''}`}
+      onClick={()=>handleFriends(index)}>{friend ? "Friend" : "Add Friend"}</button></div>
     </div>
   )
 }
