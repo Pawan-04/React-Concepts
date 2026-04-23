e0ce464a09f14b0f01192a21e0c3b878dcf2e40c
Pawan Kumar Pandey <pawan4kp@gmail.com>
2026-04-23 10:56:41 +0530

    Card.jsx - change image mini project - use useState()

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,49 @@
 import React, { useState } from 'react'
 
 const Card = () => {
-  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6])
+
+  const [val,setVal] = useState(false)
 
   return (
-    <div className="flex flex-col gap-6 items-center justify-center max-w-md mx-auto">
-      {numbers.map((number, index) => (
-        <div className="p-6 bg-gradient-to-br from-slate-100 to-slate-200 border border-slate-300 rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:scale-105 w-full" key={index}>
-          <h3 className="text-xl font-bold text-slate-800 mb-2">{number}</h3>
-          
-        </div>
-      ))}
-      <button className="px-6 py-3 bg-gradient-to-r from-red-500 to-red-600 text-white font-semibold rounded-lg shadow-md hover:shadow-lg hover:from-red-600 hover:to-red-700 transition-all duration-300 transform hover:scale-105"
+    <div className="w-full h-screen bg-zinc-300 flex justify-center items-center">
+
       
-      onClick={()=>setNumbers(()=>  numbers.filter((elem,index)=> index!=numbers.length-1))}>      Delete
-          </button>
+      <div className="w-72 bg-white rounded-xl shadow-lg p-4">
+
+       
+        <div className={`w-full h-40  overflow-hidden rounded-lg`}>
+
+          {/* Images wrapper (side by side) */}
+          <div className={`flex transition-transform duration-500 ${val? '-translate-x-full' : 'translate-x-0'}`}>
+
+            <img
+              className={`w-full h-40 object-cover flex-shrink-0 `}
+              src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d"
+              alt="img1"
+            />
+
+            {/* <img
+              className="w-full h-40 object-cover flex-shrink-0"
+              src="https://images.unsplash.com/photo-1492724441997-5dc865305da7"
+              alt="img2"
+            /> */}
+
+            <img
+              className="w-full h-40 object-cover flex-shrink-0"
+              src="https://images.unsplash.com/photo-1776255076699-d2dc33434b16?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D"
+              alt="img3"
+            />
+
+          </div>
+        </div>
+
+       
+        <button className="mt-4 w-full py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"        onClick={()=>setVal(()=>!val)}>
+          Change Image
+        </button>
+
+      </div>
+
     </div>
   )
 }
